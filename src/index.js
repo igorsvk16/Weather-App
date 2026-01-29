ebc18ab3017140047b4a6f1ad31de72359c323ac
Igor <igorsvk16@gmail.com>
2026-01-29 14:05:12 +0300

    add form for search any location weather

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import getData from "./weatherAPI.js";
 
-getData();
+
+const form = document.getElementById("location-form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inp = document.getElementById('location-input').value.trim();
+    getData(inp);
+})
